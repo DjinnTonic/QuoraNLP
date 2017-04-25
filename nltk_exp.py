bffe47ab6948dfb18209690c8ad88f94406f8f3b
Rami (dong) <rami.elrashid@gmail.com>
2017-04-25 15:47:40 +0200

    tsne for data

--- nltk_exp.py
+++ nltk_exp.py
@@ -1,9 +1,11 @@
 import nltk
-sentence = """Soon we are going to a meeting about further developments."""
+from collections import Counter
+
+sentence = """This is a sentence with a duplicate word."""
 tokens = nltk.word_tokenize(sentence)
 pos = nltk.pos_tag(tokens)
-nltk.help.upenn_tagset('VBG')
+# nltk.help.upenn_tagset('VBG') # Help on POS
 
-dpos = dict(pos)
-revd = {v: k for k, v in dpos.items()}
-[item for item in pos if 'NN' in item]
+# dpos = dict(pos)
+# revd = {v: k for k, v in dpos.items()} # Reverses keys/values
+# [item for item in pos if 'NN' in item] # Stuff
