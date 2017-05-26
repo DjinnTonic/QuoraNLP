6146a5a316fef8f535cc6eae6dbbb725a2b04ea7
Rami (dong) <rami.elrashid@gmail.com>
2017-05-26 16:10:49 +0200

    Saving data and model successful!

--- kernel.py
+++ kernel.py
@@ -33,6 +33,8 @@ from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 
+import shelve
+
 import sys
 
 #reload(sys)
@@ -50,10 +52,15 @@ MAX_NB_WORDS = 200000
 EMBEDDING_DIM = 300
 VALIDATION_SPLIT = 0.1
 
-num_lstm = np.random.randint(175, 275)
-num_dense = np.random.randint(100, 150)
-rate_drop_lstm = 0.15 + np.random.rand() * 0.25
-rate_drop_dense = 0.15 + np.random.rand() * 0.25
+#num_lstm = np.random.randint(175, 275)
+#num_dense = np.random.randint(100, 150)
+#rate_drop_lstm = 0.15 + np.random.rand() * 0.25
+#rate_drop_dense = 0.15 + np.random.rand() * 0.25
+
+num_lstm = 273
+num_dense = 121
+rate_drop_lstm = 0.18
+rate_drop_dense = 0.29
 
 act = 'relu'
 re_weight = True  # whether to re-weight classes to fit the 17.5% share in test set
@@ -62,161 +69,204 @@ STAMP = 'lstm_%d_%d_%.2f_%.2f' % (num_lstm, num_dense, rate_drop_lstm, \
                                   rate_drop_dense)
 
 ########################################
-## index word vectors
+## index word vectors and other preprocessing steps
 ########################################
-print('Indexing word vectors')
 
-word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, \
-                                             binary=True) # Takes around 2 minutes to load and run!
-print('Found %s word vectors of word2vec' % len(word2vec.vocab))
+prep = input('Import mdata (Y/N)?: ')
+if prep in ('Y', 'y'):
+    prep = input('Filename? (blank for default): ')
+    prep = 'mdata' if prep == "" else 'mdata_' + prep
+    f = shelve.open(prep)
+    data_1_train = f['data_1_train']
+    data_2_train = f['data_2_train']
+    labels_train = f['labels_train']
+    data_1_val = f['data_1_val']
+    data_2_val = f['data_2_val']
+    labels_val = f['labels_val']
+    weight_val = f['weight_val']
+    nb_words = f['nb_words']
+    embedding_matrix = f['embedding_matrix']
+    test_data_1 = f['test_data_1']
+    test_data_2 = f['test_data_2']
+    test_ids = f['test_ids']
+    f.close()
+else:
+    print('Indexing word vectors')
 
-########################################
-## process texts in datasets
-########################################
-print('Processing text dataset')
+    ##
+    word2vec = KeyedVectors.load_word2vec_format(EMBEDDING_FILE, \
+                                                 binary=True) # Takes around 2 minutes to load and run!
+    print('Found %s word vectors of word2vec' % len(word2vec.vocab))
 
-
-# The function "text_to_wordlist" is from
-# https://www.kaggle.com/currie32/quora-question-pairs/the-importance-of-cleaning-text
-def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
-    # Clean the text, with the option to remove stopwords and to stem words.
-
-    # Convert words to lower case and split them
-    text = text.lower().split()
-
-    # Optionally, remove stop words
-    if remove_stopwords:
-        stops = set(stopwords.words("english"))
-        text = [w for w in text if not w in stops]
-
-    text = " ".join(text)
-
-    # Clean the text
-    text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
-    text = re.sub(r"what's", "what is ", text)
-    text = re.sub(r"\'s", " ", text)
-    text = re.sub(r"\'ve", " have ", text)
-    text = re.sub(r"can't", "cannot ", text)
-    text = re.sub(r"n't", " not ", text)
-    text = re.sub(r"i'm", "i am ", text)
-    text = re.sub(r"\'re", " are ", text)
-    text = re.sub(r"\'d", " would ", text)
-    text = re.sub(r"\'ll", " will ", text)
-    text = re.sub(r",", " ", text)
-    text = re.sub(r"\.", " ", text)
-    text = re.sub(r"!", " ! ", text)
-    text = re.sub(r"\/", " ", text)
-    text = re.sub(r"\^", " ^ ", text)
-    text = re.sub(r"\+", " + ", text)
-    text = re.sub(r"\-", " - ", text)
-    text = re.sub(r"\=", " = ", text)
-    text = re.sub(r"'", " ", text)
-    text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
-    text = re.sub(r":", " : ", text)
-    text = re.sub(r" e g ", " eg ", text)
-    text = re.sub(r" b g ", " bg ", text)
-    text = re.sub(r" u s ", " american ", text)
-    text = re.sub(r"\0s", "0", text)
-    text = re.sub(r" 9 11 ", "911", text)
-    text = re.sub(r"e - mail", "email", text)
-    text = re.sub(r"j k", "jk", text)
-    text = re.sub(r"\s{2,}", " ", text)
-
-    # Optionally, shorten words to their stems
-    if stem_words:
-        text = text.split()
-        stemmer = SnowballStemmer('english')
-        stemmed_words = [stemmer.stem(word) for word in text]
-        text = " ".join(stemmed_words)
-
-    # Return a list of words
-    return (text)
+    ########################################
+    ## process texts in datasets
+    ########################################
+    print('Processing text dataset')
 
 
-texts_1 = []
-texts_2 = []
-labels = []
+    # The function "text_to_wordlist" is from
+    # https://www.kaggle.com/currie32/quora-question-pairs/the-importance-of-cleaning-text
+    def text_to_wordlist(text, remove_stopwords=False, stem_words=False):
+        # Clean the text, with the option to remove stopwords and to stem words.
 
-# This part takes around 5-6 minutes
-with codecs.open(TRAIN_DATA_FILE, encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',')
-    header = next(reader)
-    for values in reader:
-        texts_1.append(text_to_wordlist(values[3]))
-        texts_2.append(text_to_wordlist(values[4]))
-        labels.append(int(values[5]))
-print('Found %s texts in train.csv' % len(texts_1))
+        # Convert words to lower case and split them
+        text = text.lower().split()
 
-test_texts_1 = []
-test_texts_2 = []
-test_ids = []
-with codecs.open(TEST_DATA_FILE, encoding='utf-8') as f:
-    reader = csv.reader(f, delimiter=',')
-    header = next(reader)
-    for values in reader:
-        test_texts_1.append(text_to_wordlist(values[1]))
-        test_texts_2.append(text_to_wordlist(values[2]))
-        test_ids.append(values[0])
-print('Found %s texts in test.csv' % len(test_texts_1))
+        # Optionally, remove stop words
+        if remove_stopwords:
+            stops = set(stopwords.words("english"))
+            text = [w for w in text if not w in stops]
 
-tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
-tokenizer.fit_on_texts(texts_1 + texts_2 + test_texts_1 + test_texts_2)
+        text = " ".join(text)
 
-sequences_1 = tokenizer.texts_to_sequences(texts_1)
-sequences_2 = tokenizer.texts_to_sequences(texts_2)
-test_sequences_1 = tokenizer.texts_to_sequences(test_texts_1)
-test_sequences_2 = tokenizer.texts_to_sequences(test_texts_2)
+        # Clean the text
+        text = re.sub(r"[^A-Za-z0-9^,!.\/'+-=]", " ", text)
+        text = re.sub(r"what's", "what is ", text)
+        text = re.sub(r"\'s", " ", text)
+        text = re.sub(r"\'ve", " have ", text)
+        text = re.sub(r"can't", "cannot ", text)
+        text = re.sub(r"n't", " not ", text)
+        text = re.sub(r"i'm", "i am ", text)
+        text = re.sub(r"\'re", " are ", text)
+        text = re.sub(r"\'d", " would ", text)
+        text = re.sub(r"\'ll", " will ", text)
+        text = re.sub(r",", " ", text)
+        text = re.sub(r"\.", " ", text)
+        text = re.sub(r"!", " ! ", text)
+        text = re.sub(r"\/", " ", text)
+        text = re.sub(r"\^", " ^ ", text)
+        text = re.sub(r"\+", " + ", text)
+        text = re.sub(r"\-", " - ", text)
+        text = re.sub(r"\=", " = ", text)
+        text = re.sub(r"'", " ", text)
+        text = re.sub(r"(\d+)(k)", r"\g<1>000", text)
+        text = re.sub(r":", " : ", text)
+        text = re.sub(r" e g ", " eg ", text)
+        text = re.sub(r" b g ", " bg ", text)
+        text = re.sub(r" u s ", " american ", text)
+        text = re.sub(r"\0s", "0", text)
+        text = re.sub(r" 9 11 ", "911", text)
+        text = re.sub(r"e - mail", "email", text)
+        text = re.sub(r"j k", "jk", text)
+        text = re.sub(r"\s{2,}", " ", text)
 
-word_index = tokenizer.word_index
-print('Found %s unique tokens' % len(word_index))
+        # Optionally, shorten words to their stems
+        if stem_words:
+            text = text.split()
+            stemmer = SnowballStemmer('english')
+            stemmed_words = [stemmer.stem(word) for word in text]
+            text = " ".join(stemmed_words)
 
-data_1 = pad_sequences(sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
-data_2 = pad_sequences(sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
-labels = np.array(labels)
-print('Shape of data tensor:', data_1.shape)
-print('Shape of label tensor:', labels.shape)
+        # Return a list of words
+        return (text)
 
-test_data_1 = pad_sequences(test_sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
-test_data_2 = pad_sequences(test_sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
-test_ids = np.array(test_ids)
 
-########################################
-## prepare embeddings
-########################################
-print('Preparing embedding matrix')
+    texts_1 = []
+    texts_2 = []
+    labels = []
 
-nb_words = min(MAX_NB_WORDS, len(word_index)) + 1
+    # This part takes around 5-6 minutes
+    with codecs.open(TRAIN_DATA_FILE, encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',')
+        header = next(reader)
+        for values in reader:
+            texts_1.append(text_to_wordlist(values[3]))
+            texts_2.append(text_to_wordlist(values[4]))
+            labels.append(int(values[5]))
+    print('Found %s texts in train.csv' % len(texts_1))
 
-embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
-for word, i in word_index.items():
-    if word in word2vec.vocab:
-        embedding_matrix[i] = word2vec.word_vec(word)
-print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+    test_texts_1 = []
+    test_texts_2 = []
+    test_ids = []
+    with codecs.open(TEST_DATA_FILE, encoding='utf-8') as f:
+        reader = csv.reader(f, delimiter=',')
+        header = next(reader)
+        for values in reader:
+            test_texts_1.append(text_to_wordlist(values[1]))
+            test_texts_2.append(text_to_wordlist(values[2]))
+            test_ids.append(values[0])
+    print('Found %s texts in test.csv' % len(test_texts_1))
 
-########################################
-## sample train/validation data
-########################################
-# np.random.seed(1234)
-perm = np.random.permutation(len(data_1))
-idx_train = perm[:int(len(data_1) * (1 - VALIDATION_SPLIT))]
-idx_val = perm[int(len(data_1) * (1 - VALIDATION_SPLIT)):]
+    tokenizer = Tokenizer(num_words=MAX_NB_WORDS)
+    tokenizer.fit_on_texts(texts_1 + texts_2 + test_texts_1 + test_texts_2)
 
-data_1_train = np.vstack((data_1[idx_train], data_2[idx_train]))
-data_2_train = np.vstack((data_2[idx_train], data_1[idx_train]))
-labels_train = np.concatenate((labels[idx_train], labels[idx_train]))
+    sequences_1 = tokenizer.texts_to_sequences(texts_1)
+    sequences_2 = tokenizer.texts_to_sequences(texts_2)
+    test_sequences_1 = tokenizer.texts_to_sequences(test_texts_1)
+    test_sequences_2 = tokenizer.texts_to_sequences(test_texts_2)
 
-data_1_val = np.vstack((data_1[idx_val], data_2[idx_val]))
-data_2_val = np.vstack((data_2[idx_val], data_1[idx_val]))
-labels_val = np.concatenate((labels[idx_val], labels[idx_val]))
+    word_index = tokenizer.word_index
+    print('Found %s unique tokens' % len(word_index))
 
-weight_val = np.ones(len(labels_val))
-if re_weight:
-    weight_val *= 0.472001959
-    weight_val[labels_val == 0] = 1.309028344
+    data_1 = pad_sequences(sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
+    data_2 = pad_sequences(sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
+    labels = np.array(labels)
+    print('Shape of data tensor:', data_1.shape)
+    print('Shape of label tensor:', labels.shape)
+
+    test_data_1 = pad_sequences(test_sequences_1, maxlen=MAX_SEQUENCE_LENGTH)
+    test_data_2 = pad_sequences(test_sequences_2, maxlen=MAX_SEQUENCE_LENGTH)
+    test_ids = np.array(test_ids)
+
+    ########################################
+    ## prepare embeddings
+    ########################################
+    print('Preparing embedding matrix')
+
+    nb_words = min(MAX_NB_WORDS, len(word_index)) + 1
+
+    embedding_matrix = np.zeros((nb_words, EMBEDDING_DIM))
+    for word, i in word_index.items():
+        if word in word2vec.vocab:
+            embedding_matrix[i] = word2vec.word_vec(word)
+    print('Null word embeddings: %d' % np.sum(np.sum(embedding_matrix, axis=1) == 0))
+
+    ########################################
+    ## sample train/validation data
+    ########################################
+    # np.random.seed(1234)
+    perm = np.random.permutation(len(data_1))
+    idx_train = perm[:int(len(data_1) * (1 - VALIDATION_SPLIT))]
+    idx_val = perm[int(len(data_1) * (1 - VALIDATION_SPLIT)):]
+
+    data_1_train = np.vstack((data_1[idx_train], data_2[idx_train]))
+    data_2_train = np.vstack((data_2[idx_train], data_1[idx_train]))
+    labels_train = np.concatenate((labels[idx_train], labels[idx_train]))
+
+    data_1_val = np.vstack((data_1[idx_val], data_2[idx_val]))
+    data_2_val = np.vstack((data_2[idx_val], data_1[idx_val]))
+    labels_val = np.concatenate((labels[idx_val], labels[idx_val]))
+
+    weight_val = np.ones(len(labels_val))
+    if re_weight:
+        weight_val *= 0.472001959
+        weight_val[labels_val == 0] = 1.309028344
+
+    # Model variables  pickled (as numpy arrays or other standard) here.
+    prep = input('Name for preprocessed data to save (N/n for skipping): ')
+    if prep not in ('N', 'n'):
+        prep = 'mdata_' + prep
+        f = shelve.open(prep)
+        f['data_1_train'] = data_1_train
+        f['data_2_train'] = data_2_train
+        f['labels_train'] = labels_train
+        f['data_1_val'] = data_1_val
+        f['data_2_val'] = data_2_val
+        f['labels_val'] = labels_val
+        f['weight_val'] = weight_val
+        f['nb_words'] = nb_words
+        f['embedding_matrix'] = embedding_matrix
+        f['test_data_1'] = test_data_1
+        f['test_data_2'] = test_data_2
+        f['test_ids'] = test_ids
+        f.close()
 
 ########################################
 ## define the model structure
 ########################################
+
+
+
 embedding_layer = Embedding(nb_words,
                             EMBEDDING_DIM,
                             weights=[embedding_matrix],
@@ -265,13 +315,18 @@ early_stopping = EarlyStopping(monitor='val_loss', patience=3)
 bst_model_path = STAMP + '.h5'
 model_checkpoint = ModelCheckpoint(bst_model_path, save_best_only=True, save_weights_only=True)
 
-hist = model.fit([data_1_train, data_2_train], labels_train, \
-                 validation_data=([data_1_val, data_2_val], labels_val, weight_val), \
-                 epochs=200, batch_size=2048, shuffle=True, \
-                 class_weight=class_weight, callbacks=[early_stopping, model_checkpoint])
+# Here you should be able to save the keras model.
+
+#Training.
+#hist = model.fit([data_1_train, data_2_train], labels_train, \
+ #                validation_data=([data_1_val, data_2_val], labels_val, weight_val), \
+ #                epochs=200, batch_size=2048, shuffle=True, \
+ #                class_weight=class_weight, callbacks=[early_stopping, model_checkpoint])
 
 model.load_weights(bst_model_path)
-bst_val_score = min(hist.history['val_loss'])
+
+#bst_val_score = min(hist.history['val_loss'])
+bst_val_score = 0
 
 ########################################
 ## make the submission
